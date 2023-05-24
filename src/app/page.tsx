a938c9059e2da50d91943e96d1685fb2add83d76
george <george94sss@gmail.com>
2023-05-24 13:47:41 +0300

    24.05.23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,236 @@
-import Image from 'next/image'
+import {
+  faPhone,
+  faEnvelope,
+  faGlobe,
+  faLocationDot,
+  faGamepad,
+  faMicrophone,
+  faBook,
+  faCutlery,
+} from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import Title from "../../components/Title";
+import List from "../../components/List";
+import ContactItem, {
+  listItemType,
+} from "../../components/ListItems/ContactItem";
+import EducationItem, {
+  educationItemType,
+} from "../../components/ListItems/EducationItem";
+import LanguageItem, {
+  languageItemType,
+} from "../../components/ListItems/LanguageItem";
+import Skill, { skillType } from "../../components/ListItems/SkillItem";
+import InterestItem, {
+  interestItemType,
+} from "../../components/ListItems/InterestItem";
+import ExperienceItem, {
+  experienceItemType,
+} from "../../components/ListItems/ExperienceItem";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+const arrContact: listItemType[] = [
+  { text: "+7 980 446 04 63", icon: faPhone, id: "1" },
+  { text: "+7 980 446 04 63", icon: faEnvelope, id: "2" },
+  { text: "+7 980 446 04 63", icon: faGlobe, id: "3" },
+  { text: "+7 980 446 04 63", icon: faLinkedinIn, id: "4" },
+  { text: "+7 980 446 04 63", icon: faLocationDot, id: "5" },
+];
+
+const arrEducation: educationItemType[] = [
+  {
+    title: " Master Degree in Computer Science",
+    text: "Uneversity Name",
+    id: "1",
+  },
+  {
+    title: "Bachelor Degree in Computer Science",
+    text: "Uneversity Name",
+    id: "2",
+  },
+  { title: "Matriculation", text: "Uneversity Name", id: "3" },
+];
+
+const arrLanguage: languageItemType[] = [
+  {
+    title: "English",
+    prosent: 90,
+    id: "1",
+  },
+  {
+    title: "English",
+    prosent: 48,
+    id: "2",
+  },
+  { title: "English", prosent: 85, id: "3" },
+];
+
+const arrSkills: skillType[] = [];
+
+for (let i = 0; i < 4; i++) {
+  arrSkills.push({
+    title: "HTML",
+    prosent: (i + 1) * 16,
+    id: `${i}`,
+  });
+}
+
+const arrInterests: interestItemType[] = [
+  { text: "Gaming", icon: faGamepad, id: "1" },
+  { text: "Singing", icon: faMicrophone, id: "2" },
+  { text: "Reading", icon: faBook, id: "3" },
+  { text: "Cooking", icon: faCutlery, id: "4" },
+];
+
+const arrExperience: experienceItemType[] = [
+  {
+    title_1: "2019 - Present",
+    title_2: "Company Name",
+    title_3: "Senior UX Designer",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Tempore repudiandae beatae magni quibusdam minus laboriosamtotam deleniti, repellat, nisi obcaecati!",
+    id: "1",
+  },
+  {
+    title_1: "2019 - Present",
+    title_2: "Company Name",
+    title_3: "Senior UX Designer",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Tempore repudiandae beatae magni quibusdam minus laboriosamtotam deleniti, repellat, nisi obcaecati!",
+    id: "2",
+  },
+  {
+    title_1: "2019 - Present",
+    title_2: "Company Name",
+    title_3: "Senior UX Designer",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Tempore repudiandae beatae magni quibusdam minus laboriosamtotam deleniti, repellat, nisi obcaecati!",
+    id: "3",
+  },
+  {
+    title_1: "2019 - Present",
+    title_2: "Company Name",
+    title_3: "Senior UX Designer",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Tempore repudiandae beatae magni quibusdam minus laboriosamtotam deleniti, repellat, nisi obcaecati!",
+    id: "4",
+  },
+];
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   return {
+//     props: { data: arrSkills },
+//   };
+// };
+
+// const arrSkills: skillType[] = [
+//   {
+//     title: "HTML",
+//     prosent: `w-[${prosent}%]`,
+//     id: "1",
+//   },
+//   {
+//     title: "CSS",
+//     prosent: `w-[${prosent}%]`,
+//     id: "2",
+//   },
+//   {
+//     title: "JSS",
+//     prosent: `w-[${prosent}%]`,
+//     id: "3",
+//   },
+//   {
+//     title: "TS",
+//     prosent: 14,
+//     id: "4",
+//   },
+// ];
+
+// export default arrSkills
+
+const Home = () => (
+  <div className="bg-[#ADD8E6] flex justify-center items-center min-h-[100vh]">
+    <div className="relative w-[100%] max-w-[1000px] min-h-[1000px] bg-[#fff] m-[50px] grid grid-cols-3 shadow-[0 35px 55px rgba(0, 0, 0, 0.1)]">
+      {/* Left Side */}
+      <div className="relative bg-[#003347] p-[40px] col-span-1">
+        {/* profileText */}
+        <div className="relative flex flex-col items-center p-[20px] border-b-[1px solid rgba(255, 255, 255, 0.2)]">
+          <div className="w-[200px] h-[200px] bg-gray-600 relative rounded-[50%] overflow-hidden">
+            <img
+              className="absolute top-0 left-0 w-[100%] h-[100%] object-cover"
+              src="/cat.jpg"
+              alt=""
             />
-          </a>
+          </div>
+          <h2 className="text-[#fff] text-[1.5rem] mt-[20px] uppercase text-center font-[600] leading-[1.4rem]">
+            GGarnett <br />
+            <span className="text-[1rem] font-[300]">Web developer</span>
+          </h2>
+        </div>
+        {/* contactInfo */}
+        <div className="pt-[40px]">
+          <Title title={{ text: "Contact Info", flag: true }} />
+          <List>
+            {arrContact.map((item) => {
+              return <ContactItem listItem={item} />;
+            })}
+          </List>
+        </div>
+        {/* contactInfo education */}
+        <div className="pt-[40px]">
+          <Title title={{ text: "Education", flag: true }} />
+          <List>
+            {arrEducation.map((item) => {
+              return <EducationItem listItem={item} />;
+            })}
+          </List>
+        </div>
+        {/* contactInfo language */}
+        <div className="pt-[40px] ">
+          <Title title={{ text: "Languages", flag: true }} />
+          <List>
+            {arrLanguage.map((item) => {
+              return <LanguageItem listItem={item} />;
+            })}
+          </List>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Right Side */}
+      <div className="relative bg-[#fff] p-[40px] col-span-2">
+        <div className="mb-[50px]">
+          <Title title={{ text: "Profile", flag: false }} />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            dolores, necessitatibus aliquam natus officia deleniti quam
+            adipisci! Tempora vero temporibus odio hic. Dolorum magni tenetur
+            omnis, ex perspiciatis molestiae nobis?
+          </p>
+        </div>
+
+        {/* experience skills */}
+        <div className="mb-[50px]">
+          <Title title={{ text: "Experiance", flag: false }} />
+          {arrExperience.map((item) => {
+            return <ExperienceItem listItem={item} />;
+          })}
+        </div>
+
+        {/* about skills */}
+        <div className="mb-[50px]">
+          <Title title={{ text: "Profesioanl skils", flag: false }} />
+          {arrSkills.map((item) => {
+            return <Skill listItem={item} />;
+          })}
+        </div>
+
+        {/* about interest */}
+        <div className="mb-[50px] ">
+          <Title title={{ text: "Intersets", flag: false }} />
+          <ul className="grid grid-cols-4">
+            {arrInterests.map((item) => {
+              return <InterestItem listItem={item} />;
+            })}
+          </ul>
+        </div>
       </div>
+    </div>
+  </div>
+);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
